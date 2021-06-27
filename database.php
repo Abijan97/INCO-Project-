6b866232e1c2110560132939facf9b54be3a0b3f
ThilinaDananjaya <66713156+ThilinaDananjaya@users.noreply.github.com>
2021-06-28 03:08:49 +0530

    Update database.php

--- database.php
+++ database.php
@@ -1,7 +1,7 @@
 <?php
-    $servername = "localhost";
-    $dBUsername = "root";
-    $dBPassword = "";
+    $servername = "db4free.net";
+    $dBUsername = "dottech";
+    $dBPassword = "dottech2017";
     $dBName = "dottech";
 
     $conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
